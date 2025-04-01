b15ff0ec4b4cbe98f892a3693fddffa1d88e2369
MirkanPolat <Mirkanpolat@outlook.de>
2025-04-01 14:07:12 +0200

    adding some grids

---
+++ script.js
@@ -0,0 +1,28 @@
+function init() {
+  render();
+}
+
+function render() {
+  const contentDiv = document.getElementById("content");
+}
+
+function render() {
+    const contentDiv = document.getElementById('content');
+    let tableHtml = '<table>';
+    for (let i = 0; i < 3; i++) {
+        tableHtml += '<tr>';
+        for (let j = 0; j < 3; j++) {
+            const index = i * 3 + j;
+            let symbol = '';
+            if (fields[index] === 'circle') {
+                symbol = 'o';
+            } else if (fields[index] === 'cross') {
+                symbol = 'x';
+            }
+            tableHtml += `<td>${symbol}</td>`;
+        }
+        tableHtml += '</tr>';
+    }
+    tableHtml += '</table>';
+    contentDiv.innerHTML = tableHtml;
+}
